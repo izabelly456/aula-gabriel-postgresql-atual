794fbc61b8c0660337f11ee1653615ab14ece8df
izabelly <izabelly.e.souza@aluno.senai.br>
2025-09-30 16:15:16 -0300

    estrutura de pasta de projetos

---
+++ db.py
@@ -0,0 +1,17 @@
+import psycopg2 as pg
+#pip ijnstall psycopg2
+#pip install dotenv
+from dotenv import load_dotenv     
+import os 
+
+#carregar as variaveis de ambiente do arquivo .env
+load_dotenv()
+
+params = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+}
+
